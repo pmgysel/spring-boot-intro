03851160a27ded25fd7460591ddf9dcfc33017b6
Philipp Gysel <gyselph@gmail.com>
2020-04-30 20:28:57 +0200

    spring boot app with one rest endpoint

---
+++ src/test/java/com/example/springbootexample/PingIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.example.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PingIntegrationTest {
+	
+	@Autowired
+	private MockMvc mvc;
+		
+	@Test
+	public void testHelloWorldEndpoint() throws Exception {
+    String response = mvc
+				.perform(get("/api/ping"))
+				.andReturn().getResponse().getContentAsString();
+    	assertEquals("pong", response);
+	}
+}
